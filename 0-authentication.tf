0260bbf8c26814581edfd7589247748b0ec657bf
uchiharod <demetri413@gmail.com>
2025-06-08 23:38:44 -0400

    Making updates to authentication and backend

--- 0-authentication.tf
+++ 0-authentication.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  project = "learning-6-5"
+  project = "phantombraves"
   region  = "us-central1"
-
+  credentials = "phantombraves-8c6f6a136931.json"
 
 }
--- 01-backend.tf
+++ 01-backend.tf
@@ -1,8 +1,9 @@
 # https://www.terraform.io/language/settings/backends/gcs
 terraform {
   backend "gcs" {
-    bucket = "manningterraformstate"
+    bucket = "manningterraformstate2"
     prefix = "terraform/state"
+    credentials = "phantombraves-8c6f6a136931.json"
   }
   required_providers {
     google = {
